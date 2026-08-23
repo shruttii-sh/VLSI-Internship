bc1db19e3fd070c85bc28720b0fdc118cb12bec1
shruttii-sh <shruttiii.14@gmail.com>
2026-08-23 20:42:17 +0530

    Update sequence_detector.v

--- sequence_detector.v
+++ sequence_detector.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.08.2026 20:37:13
-// Design Name: 
-// Module Name: sequence_detector
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module sequence_detector(
 input clk,
@@ -29,26 +9,25 @@ output reg detected);
 reg [2:0] state;
 
 always @(posedge clk or posedge reset) begin
-if (reset) begin
-state <= 0;
-detected <= 0;
-end else begin
-case(state)
-0: state <= (in) ? 1 : 0;
-1: state <= (in) ? 1 : 2;
-2: state <= (in) ? 3 : 0;
-3: begin
-if (in) begin
-detected <= 1;
-state <= 1;
-end else
-state <= 2;
+    if (reset) begin
+        state <= 0;
+        detected <= 0;
+    end else begin
+        detected <= 0;
 
-end
-endcase
-
-end
-
+        case(state)
+            0: state <= (in) ? 1 : 0;
+            1: state <= (in) ? 1 : 2;
+            2: state <= (in) ? 3 : 0;
+            3: begin
+                if (in) begin
+                    detected <= 1;
+                    state <= 1;
+                end else
+                    state <= 2;
+            end
+        endcase
+    end
 end
 
 endmodule
